6eba9e5f96cdda65d04fd106d6adc52522566eef
Vitaliy.Loshniy <porsche839@gmail.com>
2021-08-21 11:45:05 +0300

    abstrct conceptual

---
+++ abstracts/conceptual/AbstractProductA.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace abstracts\conceptual;
+/**
+ * Каждый отдельный продукт семейства продуктов должен иметь базовый интерфейс.
+ * Все вариации продукта должны реализовывать этот интерфейс.
+ */
+interface AbstractProductA
+{
+    public function usefulFunctionA(): string;
+}
---
+++ abstracts/conceptual/abstract-conceptual.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace abstracts\conceptual;
+
+spl_autoload_register(function ($path) {
+    $className = end(explode('\\', $path));
+    require_once $className . '.php';
+});
+
+
+/**
+ * Клиентский код работает с фабриками и продуктами только через абстрактные
+ * типы: Абстрактная Фабрика и Абстрактный Продукт. Это позволяет передавать
+ * любой подкласс фабрики или продукта клиентскому коду, не нарушая его.
+ */
+function clientCode(AbstractFactory $factory)
+{
+    $productA = $factory->createProductA();
+    $productB = $factory->createProductB();
+
+    echo $productB->usefulFunctionB() . "\n";
+    echo $productB->anotherUsefulFunctionB($productA) . "\n";
+}
+
+/**
+ * Клиентский код может работать с любым конкретным классом фабрики.
+ */
+echo "Client: Testing client code with the first factory type:\n";
+clientCode(new ConcreteFactory1());
+
+echo "<br>";
+
+echo "Client: Testing the same client code with the second factory type:\n";
+clientCode(new ConcreteFactory2());
