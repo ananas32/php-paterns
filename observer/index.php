e5d2d9f0dd9017221a3871b14cc9d17bac6e1df6
Vitaliy.Loshniy <porsche839@gmail.com>
2021-08-22 12:34:27 +0300

    add observer pattern

---
+++ observer/index.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace observer;
+
+include '../autoload.php';
+
+/**
+ * Клиентский код.
+ */
+
+$subject = new Subject();
+
+$o1 = new ConcreteObserverA();
+$subject->attach($o1);
+
+$o2 = new ConcreteObserverB();
+$subject->attach($o2);
+
+$subject->someBusinessLogic();
+$subject->someBusinessLogic();
+
+$subject->detach($o2);
+
+$subject->someBusinessLogic();
